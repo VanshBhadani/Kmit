ee10aae995364497f7148d382c39df4dbbd20bfe
Vansh Bhadani <vanshbhadani2005@gmail.com>
2024-11-30 19:37:18 +0530

    alpha1

---
+++ app/hervival/lib/pages/login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}

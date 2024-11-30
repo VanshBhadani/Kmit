74f73161e544e180da6a8052edd583ec0defd984
Vansh Bhadani <vanshbhadani2005@gmail.com>
2024-12-01 01:13:52 +0530

    progress 2

--- app/hervival/lib/main.dart
+++ app/hervival/lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:hervival/pages/login_page.dart';
+import 'pages/login_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );

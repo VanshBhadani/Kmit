4f02ba0fcd2fd51601d1974bf32e303c1d8a655f
Vansh Bhadani <vanshbhadani2005@gmail.com>
2024-12-01 08:08:43 +0530

    phase 1

--- app/hervival/lib/pages/login_page.dart
+++ app/hervival/lib/pages/login_page.dart
@@ -4,15 +4,29 @@ import 'package:hervival/components/my_button.dart';
 import 'package:hervival/components/my_textfield.dart';
 import 'package:hervival/components/square_tile.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   // text editing controllers
   final emailController = TextEditingController();
+
   final passwordController = TextEditingController();
 
   // sign user in method
   void signUserIn() async{
+
+    showDialog(context: context, builder: (context) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+      },
+    );
+
     await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: emailController.text,
         password: passwordController.text,
